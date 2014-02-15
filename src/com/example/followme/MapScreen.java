4d4ecbd58f4194d05284ca2da43a5b14ac155f43
Devin Caplow-Munro <decamun@gmail.com>
2014-02-15 03:08:17 -0500

    added a primitive login screen

--- src/com/example/followme/MapScreen.java
+++ src/com/example/followme/MapScreen.java
@@ -1,15 +1,24 @@
 package com.example.followme;
 
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MapScreen extends Activity {
 
+	boolean logged_in = false;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_screen);
+        if(!logged_in) {
+        	Intent intent = new Intent(this, LoginActivity.class);
+    		startActivity(intent);
+        }
     }
 
 
